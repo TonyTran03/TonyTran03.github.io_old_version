b8241953986fd6abeeefeae8c7bde9fba5c9601e
TonyTran03 <tony.tran03@hotmail.com>
2024-07-11 04:41:12 -0400

    no font

--- src/components/three/Wproject.jsx
+++ src/components/three/Wproject.jsx
@@ -1,9 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { Text, Box, RenderTexture, Environment } from '@react-three/drei';
-import { PointsMaterial } from 'three';
-import { Dotted } from './Dotted';
-import { Polaroid } from './Polaroid';
-import { Rocketship } from './Rocketship';
 
 export function Wproject() {
     // Define the material properties for the points
@@ -15,7 +11,7 @@ export function Wproject() {
             <Text 
                 position={[-22,-17, 1]} // Adjust position based on the size and placement of the box
                 fontSize={2}
-                font={"/fonts/Poppins-Regular.ttf"} 
+              
             >
                 P r o j e c t s
 
